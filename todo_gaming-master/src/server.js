e4e737488183a3cfd827118641752a0422b2ce68
vladimire09 <joshuasimp83@gmail.com>
2026-02-28 19:35:10 -0500

    Actualizando carpeta con nuevos archivos

--- todo_gaming-master/src/server.js
+++ todo_gaming-master/src/server.js
@@ -3,8 +3,7 @@ setServers(["1.1.1.1", "8.8.8.8"]);
 
 import app from "./app.js"; 
 // Usamos "../" porque db.js está una carpeta ARRIBA de server.js
-import connectDB from "../db.js"; 
-
+import connectDB from "./config/db.js"; 
 connectDB();
 
 // Usar el puerto que asigne Render o el 3000 por defecto
